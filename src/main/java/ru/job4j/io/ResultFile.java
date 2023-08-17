84705364e6740b89b9486e7722bbc38f51097f00
SergeyDevStart <vasenev.front@gmail.com>
2023-08-18 06:34:07 +0700

    5.2. BufferedOutputStream [#252490] добавление PrintWriter

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,15 +1,19 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("data/result.txt")) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("data/result.txt")
+                ))) {
             for (int index = 2; index < 10; index++) {
                 String str = String.format("1 * %d = %d", index, index);
-                out.write(str.getBytes());
-                out.write(System.lineSeparator().getBytes());
+                out.println(str);
             }
         } catch (IOException e) {
             e.getStackTrace();
