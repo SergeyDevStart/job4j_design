68ec7034a891e88cfc645a1f79820d822e9a74bc
SergeyDevStart <vasenev.front@gmail.com>
2023-09-20 20:50:25 +0700

    7. Анализ доступности сервера [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,2 +1,32 @@
-package ru.job4j.io;public class Analysis {
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(
+                     new BufferedWriter(
+                             new FileWriter(target)
+                     ))) {
+            boolean worker = true;
+            String[] lines = in.lines().toArray(String[]::new);
+            for (String line : lines) {
+                if (worker && (line.startsWith("500") || line.startsWith("400"))) {
+                    worker = false;
+                    out.printf("%s%s", line.substring(4), ";");
+                } else if (!worker && (line.startsWith("200") || line.startsWith("300"))) {
+                    out.printf("%s%n", line.substring(4));
+                    worker = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
 }
