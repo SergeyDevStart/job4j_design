a285735937b73563c608477230a6ca5a6e3a7144
SergeyDevStart <vasenev.front@gmail.com>
2023-10-01 16:18:02 +0700

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -7,13 +7,26 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("./data");
-        search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+        verification(args);
+        Path start = Path.of(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void verification(String[] args) {
+        if (args.length == 0 || args.length > 2) {
+            throw new IllegalArgumentException("Use two parameters: ROOT_FOLDER FILE_EXTENSION");
+        }
+        if (!Files.isDirectory(Path.of(args[0]))) {
+            throw new IllegalArgumentException("Such directory does not exist. Use ROOT_FOLDER.");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("For second parameter use this format: \".FILE_EXTENSION\"");
+        }
     }
 }
