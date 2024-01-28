04f9b7d81f97f8bcf52a9b03df532b78f57dce35
SergeyDevStart <vasenev.front@gmail.com>
2024-01-28 14:29:02 +0700

    1. Отчеты. [#850]

---
+++ src/main/java/ru/job4j/ood/srp/report/HRReport.java
@@ -0,0 +1,35 @@
+package ru.job4j.ood.srp.report;
+
+import ru.job4j.ood.srp.model.Employee;
+import ru.job4j.ood.srp.store.Store;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class HRReport implements Report {
+    private final Store store;
+
+    public HRReport(Store store) {
+        this.store = store;
+    }
+
+    @Override
+    public String generate(Predicate<Employee> filter) {
+        StringBuilder text = new StringBuilder();
+        text.append("Name; Salary;")
+                .append(System.lineSeparator());
+        List<Employee> tempList = store.findBy(filter);
+        tempList.sort(byDescSalary());
+        for (Employee employee : tempList) {
+            text.append(employee.getName()).append(" ")
+                    .append(employee.getSalary())
+                    .append(System.lineSeparator());
+        }
+        return text.toString();
+    }
+
+    private static Comparator<Employee> byDescSalary() {
+        return (e1, e2) -> Double.compare(e2.getSalary(), e1.getSalary());
+    }
+}
---
+++ src/test/java/ru/job4j/ood/srp/report/HRReportTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.ood.srp.report;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.ood.srp.model.Employee;
+import ru.job4j.ood.srp.store.MemoryStore;
+import ru.job4j.ood.srp.store.Store;
+
+import java.util.Calendar;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HRReportTest {
+    @Test
+    public void whenHRGenerated() {
+        Store store = new MemoryStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker1 = new Employee("Ivan", now, now, 100);
+        Employee worker2 = new Employee("Alex", now, now, 150);
+        Employee worker3 = new Employee("Sergey", now, now, 200);
+        store.add(worker2);
+        store.add(worker3);
+        store.add(worker1);
+        Report engine = new HRReport(store);
+        StringBuilder expected = new StringBuilder()
+                .append("Name; Salary;")
+                .append(System.lineSeparator())
+                .append(worker3.getName()).append(" ")
+                .append(worker3.getSalary())
+                .append(System.lineSeparator())
+                .append(worker2.getName()).append(" ")
+                .append(worker2.getSalary())
+                .append(System.lineSeparator())
+                .append(worker1.getName()).append(" ")
+                .append(worker1.getSalary())
+                .append(System.lineSeparator());
+        assertThat(engine.generate(employee -> true)).isEqualTo(expected.toString());
+    }
+}
